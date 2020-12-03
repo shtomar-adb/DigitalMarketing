1e50d20aceafe107ae528322fda6f0dcfdfe2858
Babu Rajendran <babu.rajendran@sjsu.edu>
2020-12-03 09:27:22 +0530

    Update DynamoDbConfiguration.java

--- admin-portal/src/main/java/com/digital/marketing/config/DynamoDbConfiguration.java
+++ admin-portal/src/main/java/com/digital/marketing/config/DynamoDbConfiguration.java
@@ -6,11 +6,21 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDbConfiguration {
+
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+    @Value("${amazonProperties.dynamoDBEndpoint}")
+    private String dynamoDBEndpoint;
+    @Value("${amazonProperties.signingRegion}")
+    private String signingRegion;
 
     @Bean
     public DynamoDBMapper dynamoDBMapper() {
@@ -21,16 +31,9 @@ public class DynamoDbConfiguration {
         return AmazonDynamoDBClientBuilder
                 .standard()
                 .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(
-                                "dynamodb.us-west-2.amazonaws.com",
-                                "us-west-2"
-                        )
-                )
+                        new AwsClientBuilder.EndpointConfiguration(dynamoDBEndpoint, signingRegion))
                 .withCredentials(new AWSStaticCredentialsProvider(
-                        new BasicAWSCredentials(
-                                "AKIATCWFFZKUTKMW7RYW",
-                                "FwpDhXhnkFqyaGI5iv1KOH39x+dFXkrux+NVQtPe\n"
-                        )
+                        new BasicAWSCredentials( accessKey, secretKey)
                 ))
                 .build();
     }
